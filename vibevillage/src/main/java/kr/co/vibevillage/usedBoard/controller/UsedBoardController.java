638721bfbf7b8664fbf694f628e22268a0bf6296
ES <coka0602@hs.ac.kr>
2024-08-11 18:36:59 +0900

    중고 거래 게시판 기능 추가
    [feat] : 게시글 댓글 기능 추가
    
    1. 요소 하단에 댓글 조회와 입력을 배치했습니다.
    2. 댓글 서비스와 매퍼를 따로 만들었습니다.

--- vibevillage/src/main/java/kr/co/vibevillage/usedBoard/controller/UsedBoardController.java
+++ vibevillage/src/main/java/kr/co/vibevillage/usedBoard/controller/UsedBoardController.java
@@ -2,9 +2,11 @@ package kr.co.vibevillage.usedBoard.controller;
 
 import kr.co.vibevillage.common.UploadFile;
 import kr.co.vibevillage.common.UsedPagination;
+import kr.co.vibevillage.usedBoard.model.UsedBoardCommentDto;
 import kr.co.vibevillage.usedBoard.model.UsedBoardDto;
 import kr.co.vibevillage.usedBoard.model.UsedBoardImageDto;
 import kr.co.vibevillage.usedBoard.model.UsedPageInfoDto;
+import kr.co.vibevillage.usedBoard.service.UsedBoardCommentServiceImpl;
 import kr.co.vibevillage.usedBoard.service.UsedBoardServiceImpl;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,11 +27,13 @@ import java.util.Map;
 @Controller
 @RequestMapping("/used")
 public class UsedBoardController {
+        private final UsedBoardCommentServiceImpl commentService;
         private final UsedPagination usedPagination;
         private final Env env;
         private final UploadFile uploadFile;
         private final UsedBoardServiceImpl usedBoardService;
         private final String USE = "usedBoard/usedBoard";
+    private final UsedBoardCommentServiceImpl usedBoardCommentServiceImpl;
 
     public String kakaoMap;
 
@@ -37,12 +41,15 @@ public class UsedBoardController {
         public UsedBoardController(UsedBoardServiceImpl usedBoardService,
                                    UploadFile uploadFile,
                                    Env env,
-                                   UsedPagination usedPagination){
+                                   UsedPagination usedPagination,
+                                   UsedBoardCommentServiceImpl commentService, UsedBoardCommentServiceImpl usedBoardCommentServiceImpl){
             this.usedBoardService = usedBoardService;
             this.uploadFile = uploadFile;
             this.env = env;
             this.kakaoMap = env.test().get("KAKAOMAP");
             this.usedPagination = usedPagination;
+            this.commentService = commentService;
+            this.usedBoardCommentServiceImpl = usedBoardCommentServiceImpl;
         }
 // 게시글 리스트 조회
     @GetMapping("/boardList/{cpage}")
@@ -95,21 +102,33 @@ public class UsedBoardController {
 
 // 게시글 조회
         @GetMapping("/boardDetail/{id}")
-         public String getUsedBoardDetail(Model model, @PathVariable("id") String id){
+         public String getUsedBoardDetail(Model model,
+                                          @PathVariable("id") String id,
+                                          UsedBoardCommentDto comment){
             int boardId = Integer.parseInt(id);
             UsedBoardDto board = usedBoardService.getUsedBoardDetail(boardId);
 
             List<UsedBoardImageDto> mainImages = usedBoardService.getUsedBoardMainImage(boardId);
             List<UsedBoardImageDto> subImages = usedBoardService.getUsedBoardSubImage(boardId);
+            List<UsedBoardCommentDto> commentList = commentService.getCommentList(boardId);
         model.addAttribute("usedBoard", board);
         model.addAttribute("mainImages", mainImages);
         model.addAttribute("subImages", subImages);
+        model.addAttribute("comment",comment);
+        model.addAttribute("commentList",commentList);
+        model.addAttribute("commentSize",commentList.size());
 
         return USE+"Detail";
         }
-// 게시글 수정 페이지 이동
-//    public String updateUsedBoard(){
-//
-//    }
+// 게시글 댓글 작성
+    @PostMapping("/boardDetail/{id}/put")
+    public String putUsedBoardComment(@PathVariable("id") int id,
+                                      @ModelAttribute("comment")UsedBoardCommentDto comment){
+        System.out.println(id+"보드 아이디");
+        System.out.println(comment.getUsedCommentContent());
+        comment.setUsedBoardId(id);
+        commentService.putComment(comment);
+    return "redirect:/used/boardDetail/"+id;
+    }
 
 }
